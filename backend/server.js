9160696a6209ab8da2c167f4d32fef0d6397752f
Kellan Brown <kbrown21@unca.edu>
2026-03-31 15:56:44 -0400

    Prep for railway production  deploy.

--- backend/server.js
+++ backend/server.js
@@ -207,11 +207,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-const path = require("path");
-
-// Serve React build files
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-});
