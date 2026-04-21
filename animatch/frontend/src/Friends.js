9129de9b26d405cc89dfad622dfd35cd886272fb
Kellan Brown <kbrown21@unca.edu>
2026-04-20 22:21:25 -0400

    this might break everyhting..

--- animatch/frontend/src/Friends.js
+++ animatch/frontend/src/Friends.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useToast } from "./Toast";
+import { useToast } from "./components/Toast";
 
 const API = "https://animatch-ofks.onrender.com";
 
